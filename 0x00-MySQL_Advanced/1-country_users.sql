949f49a1af9f382b6310f38828ebec756fbc37e5
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-08 20:53:05 +0000

    country users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Create a table users following these requirements.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
