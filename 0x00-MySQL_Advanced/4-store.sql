83c36df9a876207cc711e88fb924f69decf2abc9
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-08 22:35:42 +0000

    store

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,12 +1,9 @@
 -- Drop the trigger if it already exists
 DROP TRIGGER IF EXISTS decrease_quantity_after_order;
-
--- Create the trigger
 CREATE TRIGGER decrease_quantity_after_order
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    -- Decrease the quantity of the item in the items table
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
