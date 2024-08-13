10ba69e870029c256694899b22212e22a4ef6c45
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-08-13 14:50:12 -0700

    update topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,5 +5,6 @@ This module changes all topics of a school document based on the name
 
 
 def update_topics(mongo_collection, name, topics):
+    """This function updates all topics of a school document"""
     result = mongo_collection.update_many(name, topics)
     return result
